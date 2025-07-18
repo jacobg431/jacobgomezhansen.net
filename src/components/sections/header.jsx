8b1cdbc475d62527b73f0be57d8313af34928daa
Jacob Gomez Hansen <jacobg431@gmail.com>
2025-07-18 12:37:05 +0200

    Added reverse order animation to header, not responsive

--- src/components/sections/header.jsx
+++ src/components/sections/header.jsx
@@ -1,9 +1,11 @@
 function Header() {
     return (
         <>
-            <header className="flex items-center justify-between p-10 bg-stone-300 gap-8">
-                <div className="text-4xl font-bold">Welcome! I'm Jacob, Fullstack Developer</div>
-                <div className="w-32 min-w-32 h-32 min-h-32 rounded-full bg-stone-800"></div>
+            <header className="group flex items-center justify-between p-10 bg-stone-300 gap-8">
+                <div className="group-hover:translate-x-full text-4xl font-bold transition-all duration-500 ease-in-out">
+                    Welcome! I'm Jacob, Fullstack Developer
+                </div>
+                <div className="group-hover:-translate-x-300 w-32 min-w-32 h-32 min-h-32 rounded-full bg-stone-800 transition-all duration-500 ease-in-out"></div>
             </header>
         </>
     )
--- src/components/sections/navbar.jsx
+++ src/components/sections/navbar.jsx
@@ -1,4 +1,4 @@
-import NavbarItem from "../items/navbarItem"
+import NavbarItem from '../items/navbarItem'
 
 function Navbar() {
     return (
@@ -6,7 +6,7 @@ function Navbar() {
             <nav className="flex bg-black text-white font-bold p-10 justify-between">
                 <div className="text-lg">LOGO HERE</div>
                 <div className="flex gap-4">
-                    <NavbarItem title="About" href="#intro"/>
+                    <NavbarItem title="About" href="#intro" />
                     <NavbarItem title="Projects" href="#portfolio" />
                     <NavbarItem title="Skills" href="#expertise" />
                     <NavbarItem title="Library" href="#library" />
