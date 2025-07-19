a58ff1e9c792873aca70109fbfb3e7f6cda741e3
Jacob Gomez Hansen <jacobg431@gmail.com>
2025-07-19 10:03:48 +0200

    Gave up on react-flip-move, changed to framer-mation

--- src/components/sections/header.jsx
+++ src/components/sections/header.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
-import FlipMove from '../animations/flipMove'
-
+import { motion, AnimatePresence } from 'framer-motion'
 function Header() {
     const [flipped, setFlipped] = useState(false)
+    const items = flipped ? [
+        'avatar', 'text'
+    ] : [
+        'text', 'avatar'
+    ]
 
     function onHeaderMouseOver() {
         setFlipped(true)
@@ -12,19 +16,19 @@ function Header() {
         setFlipped(false)
     }
 
-    const elements = flipped
-        ? [
-              <div key={`avatar`} className="w-32 min-w-32 h-32 min-h-32 rounded-full bg-stone-800"></div>,
-              <div key={`text`} className="text-4xl font-bold">
-                  Welcome! I'm Jacob, Fullstack Developer
-              </div>,
-          ]
-        : [
-              <div key={`text`} className="text-4xl font-bold">
-                  Welcome! I'm Jacob, Fullstack Developer
-              </div>,
-              <div key={`avatar`} className="w-32 min-w-32 h-32 min-h-32 rounded-full bg-stone-800"></div>,
-          ]
+    //const elements = flipped
+    //    ? [
+    //          <div key={`avatar`} className="w-32 min-w-32 h-32 min-h-32 rounded-full bg-stone-800"></div>,
+    //          <div key={`text`} className="text-4xl font-bold">
+    //              Welcome! I'm Jacob, Fullstack Developer
+    //          </div>,
+    //      ]
+    //    : [
+    //          <div key={`text`} className="text-4xl font-bold">
+    //              Welcome! I'm Jacob, Fullstack Developer
+    //          </div>,
+    //          <div key={`avatar`} className="w-32 min-w-32 h-32 min-h-32 rounded-full bg-stone-800"></div>,
+    //      ]
 
     return (
         <>
@@ -34,9 +38,18 @@ function Header() {
                 onMouseOver={onHeaderMouseOver}
                 onMouseOut={onHeaderMouseOut}
             >
-                <FlipMove typeName={null} duration={500}>
-                    {elements}
-                </FlipMove>
+                <AnimatePresence>
+                    {items.map((id) => (
+                        <motion.div 
+                            key={id} 
+                            layout 
+                            transition={{ duration: 0.4 }}
+                            className={id === 'avatar' ? 'w-32 h-32 rounded-full bg-stone-800' : 'text-4xl font-bold'} 
+                        >
+                            {id === 'text' && "Welcome! I'm Jacob, Fullstack Developer"}
+                        </motion.div>
+                    ))}
+                </AnimatePresence>
             </header>
         </>
     )
