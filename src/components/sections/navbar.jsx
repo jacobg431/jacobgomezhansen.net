078c328fa7e5ea9da26839657d6c4865f2c3c90a
Jacob Gomez Hansen <jacobg431@gmail.com>
2025-07-19 12:21:51 +0200

    Added personal logo

--- src/components/sections/navbar.jsx
+++ src/components/sections/navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
     return (
         <>
             <nav className="flex bg-black text-white font-bold p-10 justify-between">
-                <div className="text-lg">LOGO HERE</div>
+                <div className="w-32 bg-center bg-contain bg-no-repeat bg-[url(/src/assets/Personal-Logo-Trans-Negative-256-Px.webp)]"></div>
                 <div className="flex gap-4">
                     <NavbarItem title="About" href="#intro" />
                     <NavbarItem title="Projects" href="#portfolio" />
