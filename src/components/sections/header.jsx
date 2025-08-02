a74625b74618c682e60de91b2b8375bb1c57585c
hanja <jacobg431@gmail.com>
2025-08-02 10:54:05 +0200

    Navbar + header responsive down to 480px

--- src/components/sections/header.jsx
+++ src/components/sections/header.jsx
@@ -1,14 +1,15 @@
 import { useMemo, useState } from 'react'
 import { motion as Motion, AnimatePresence } from 'framer-motion'
 import { useTypewriter } from 'react-simple-typewriter'
+import { setupDeviceDetectionEvents } from '../../utils/setupEventListeners'
 
 function Header() {
     const [flipped, setFlipped] = useState(false)
     const items = flipped ? ['avatar', 'text'] : ['text', 'avatar']
 
     const avatarStyling =
-        'w-32 h-32 rounded-full border-4 bg-center bg-contain bg-[url(/src/assets/Profile-Picture-256-Px.webp)]'
-    const textStyling = 'inline-block whitespace-pre text-4xl font-mono font-bold'
+        'min-w-32 h-32 rounded-full border-4 bg-center bg-contain bg-[url(/src/assets/Profile-Picture-256-Px.webp)]'
+    const textStyling = 'inline-block text-2xl sm:text-3xl lg:text-4xl font-mono font-bold whitespace-normal'
 
     const typewriterStrings = ['Software Developer', 'Embedded Programmer', 'Computer Engineer']
     const typewriterLength = Math.max(...typewriterStrings.map((s) => s.length))
@@ -24,11 +25,15 @@ function Header() {
         return typewriterText + '\u00A0'.repeat(padCount)
     }, [typewriterText, typewriterLength])
 
-    function onHeaderMouseOver() {
+    const device = setupDeviceDetectionEvents()
+
+    function onHeaderMouseEnter() {
+        console.log(device)
+        if (device !== 'Desktop') return
         setFlipped(true)
     }
 
-    function onHeaderMouseOut() {
+    function onHeaderMouseLeave() {
         setFlipped(false)
     }
 
@@ -38,8 +43,8 @@ function Header() {
                 id="header"
                 className="flex items-center p-10 border-b-4 border-b-red gap-8 relative hover:bg-black duration-400"
                 style={{ justifyContent: flipped ? 'center' : 'space-between' }}
-                onMouseOver={onHeaderMouseOver}
-                onMouseOut={onHeaderMouseOut}
+                onMouseEnter={onHeaderMouseEnter}
+                onMouseLeave={onHeaderMouseLeave}
             >
                 <AnimatePresence>
                     {items.map((id) => (
@@ -50,8 +55,15 @@ function Header() {
                             transition={{ duration: 0.4, ease: 'easeOut' }}
                             className={id === 'avatar' ? avatarStyling : textStyling}
                         >
-                            {id === 'text' && "Welcome! I'm Jacob, "}
-                            {id === 'text' && typewriterPaddedText}
+                            { id === 'text' && (
+                                <>
+                                    Welcome! I'm Jacob,{' '}
+                                    <span className="inline-block w-[10em] md:w-auto min-h-[2.7em] md:min-h-0 items-center whitespace-normal">
+                                        {typewriterPaddedText}
+                                    </span>
+                                </>
+                            )}
+
                         </Motion.div>
                     ))}
                 </AnimatePresence>
--- src/utils/setupEventListeners.jsx
+++ src/utils/setupEventListeners.jsx
@@ -1,3 +1,27 @@
+import { useState, useEffect } from "react";
+
+export function setupDeviceDetectionEvents() {
+    
+    const [device, setDevice] = useState('')
+    useEffect(() => {
+        const handleDeviceDetection = () => {
+            const userAgent = navigator.userAgent.toLowerCase()
+            const isMobile = /iphone|ipad|ipod|android|blackberry|windows phone/g.test(userAgent)
+            const isTablet = /(ipad|tablet|playbook|silk)|(android(?!.*mobile))/g.test(userAgent)
+            if (isMobile) setDevice('Mobile')
+            else if (isTablet) setDevice('Tablet')
+            else setDevice('Desktop')
+        }
+
+        handleDeviceDetection()
+        window.addEventListener('resize', handleDeviceDetection)
+
+        return () => window.removeEventListener('resize', handleDeviceDetection)
+    }, [])
+
+    return device
+}
+
 export function setupNavbarClickEvents(elementId) {
     const container = document.getElementById(elementId)
     if (!container) return () => {}
