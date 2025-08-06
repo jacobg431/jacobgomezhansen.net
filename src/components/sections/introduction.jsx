cfd64cb235b0e3531b498cc0d0110e54382a167f
hanja <jacobg431@gmail.com>
2025-08-06 19:41:58 +0200

    Replaced planet icons with more suitable icons

--- src/components/sections/introduction.jsx
+++ src/components/sections/introduction.jsx
@@ -1,6 +1,8 @@
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-//import { library } from '@fortawesome/fontawesome-svg-core'
-//import { faExternalLinkSquare } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSquareLinkedin, faSquareGithub } from '@fortawesome/free-brands-svg-icons'
+import { faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 import greenPlanet from '../../assets/Green-Black-Planet.svg'
 import redPlanet from '../../assets/Red-Black-Planet.svg'
 import yellowPlanet from '../../assets/Yellow-Black-Planet.svg'
@@ -8,9 +10,72 @@ import yellowPlanet from '../../assets/Yellow-Black-Planet.svg'
 function Introduction() {
 
     const introSectionStyling = 'flex flex-col lg:flex-row w-full gap-8 px-4 py-10 2xs:p-10 bg-white img.hover:bg-black group'
-    const planetStyling = 'h-24 xs:h-28 sm:h-32 cursor-pointer hover:scale-150 duration-400 ease-out peer z-10'
+    const iconStyling = 'cursor-pointer fill-red scale-400 hover:scale-600 duration-400 ease-out peer z-10'
     const tooltipStyling = 'top-16 peer-hover:top-32 absolute px-3 py-1 text-white bg-black rounded-2xl opacity-0 peer-hover:opacity-100 duration-400 ease-out'
-    const planetContainerStyling = 'relative flex justify-center z-5'
+    const iconContainerStyling = 'relative flex w-32 h-32 items-center justify-center z-5'
+    
+    const [isHoveringLinkedIn, setHoveringLinkedIn] = useState(false)
+    const [isHoveringGithub, setHoveringGithub] = useState(false)
+    const [isHoveringDownload, setHoveringDownload] = useState(false)
+
+    const linkedInIconStyling = { color: isHoveringLinkedIn ? "#0077b5" : "#292524" }
+    const guthubIconStyling = { color: isHoveringGithub ? "#000" : "#292524" }
+    const downloadIconStyling = { color: isHoveringDownload ? "#ff6167" : "#292524" }
+
+    library.add(faSquareLinkedin, faSquareGithub, faFileArrowDown)
+
+    const linkedInIcon = <FontAwesomeIcon 
+        icon={['fab', 'square-linkedin']} 
+        className={iconStyling} 
+        style={linkedInIconStyling} 
+        onMouseEnter={() => {return onIconMouseEnter("linkedin")}} 
+        onMouseLeave={() => {return onIconMouseLeave("linkedin")}} 
+    />
+    const githubIcon = <FontAwesomeIcon 
+        icon={['fab', 'square-github']} 
+        className={iconStyling} 
+        style={guthubIconStyling}
+        onMouseEnter={() => onIconMouseEnter("github")}
+        onMouseLeave={() => onIconMouseLeave("github")}
+    />
+    const downloadIcon = <FontAwesomeIcon 
+        icon={'file-arrow-down'} 
+        className={iconStyling} 
+        style={downloadIconStyling} 
+        onMouseEnter={() => onIconMouseEnter("download")}
+        onMouseLeave={() => onIconMouseLeave("download")}
+    />
+
+    
+    function onIconMouseEnter(icon) {
+        switch(icon) {
+            case "linkedin":
+                setHoveringLinkedIn(true)
+                break
+            case "github":
+                setHoveringGithub(true)
+                break
+            case "download":
+                setHoveringDownload(true)
+                break
+        }
+    }
+
+    function onIconMouseLeave(icon) {
+        switch(icon) {
+            case "linkedin":
+                setHoveringLinkedIn(false)
+                console.log(isHoveringLinkedIn)
+                break
+            case "github":
+                setHoveringGithub(false)
+                break
+            case "download":
+                setHoveringDownload(false)
+                break
+        }
+    }
+
     return (
         <>
             <section id="intro" className={introSectionStyling}>
@@ -23,18 +88,17 @@ function Introduction() {
                     growth opportunities.
                 </p>
                 <div className="flex flex-row w-full items-center justify-around">
-                    <span className="absolute text-[6rem] text-gray opacity-0 group-hover:opacity-50 duration-400 ease-out">Socials</span>
-                    <div className={planetContainerStyling}>
-                        <img src={greenPlanet} alt="Flat vector image depicting a green planet" className={planetStyling} />
+                    <div className={iconContainerStyling}>
+                        {linkedInIcon}
                         <div className={tooltipStyling}>GitHub</div>
                     </div>
-                    <div className={planetContainerStyling}>
-                        <img src={redPlanet} alt="Flat vector image depicting a yellow planet with rings" className={planetStyling} />
+                    <div className={iconContainerStyling}>
+                        {githubIcon}
                         <div className={tooltipStyling}>LinkedIn</div>
                     </div>
-                    <div className={planetContainerStyling}>
-                        <img src={yellowPlanet} alt="Flat vector image depicting a red planet" className={planetStyling} />
-                        <div className={tooltipStyling}>Message Me</div>
+                    <div className={iconContainerStyling}>
+                        {downloadIcon}
+                        <div className={tooltipStyling}>Download Resume</div>
                     </div>
                 </div>
             </section>
