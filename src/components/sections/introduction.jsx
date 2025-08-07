320afe35f940dac45bef5d5bb1ee65b4e054ace9
Jacob Gomez Hansen <jacobg431@gmail.com>
2025-08-07 09:14:52 +0200

    Fully responsive down to 320 px

--- src/components/sections/introduction.jsx
+++ src/components/sections/introduction.jsx
@@ -3,74 +3,85 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSquareLinkedin, faSquareGithub } from '@fortawesome/free-brands-svg-icons'
 import { faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
-import greenPlanet from '../../assets/Green-Black-Planet.svg'
-import redPlanet from '../../assets/Red-Black-Planet.svg'
-import yellowPlanet from '../../assets/Yellow-Black-Planet.svg'
+import { useDeviceDetection } from '../../hooks/useDeviceDetection'
 
 function Introduction() {
+    const introSectionStyling =
+        'flex flex-col lg:flex-row w-full gap-8 px-4 pt-10 2xs:p-10 bg-white img.hover:bg-black group'
+    const iconStyling = 'cursor-pointer scale-300 xs:scale-400 hover:scale-450 xs:hover:scale-600 duration-400 ease-out peer z-10'
+    const tooltipStyling =
+        'top-16 peer-hover:top-32 absolute px-3 py-1 text-white bg-black rounded-2xl opacity-0 peer-hover:opacity-100 duration-400 ease-out'
+    const iconContainerStyling = 'relative flex w-18 h-18 xs:w-24 xs:h-24 items-center justify-center z-5'
 
-    const introSectionStyling = 'flex flex-col lg:flex-row w-full gap-8 px-4 py-10 2xs:p-10 bg-white img.hover:bg-black group'
-    const iconStyling = 'cursor-pointer fill-red scale-400 hover:scale-600 duration-400 ease-out peer z-10'
-    const tooltipStyling = 'top-16 peer-hover:top-32 absolute px-3 py-1 text-white bg-black rounded-2xl opacity-0 peer-hover:opacity-100 duration-400 ease-out'
-    const iconContainerStyling = 'relative flex w-32 h-32 items-center justify-center z-5'
-    
     const [isHoveringLinkedIn, setHoveringLinkedIn] = useState(false)
     const [isHoveringGithub, setHoveringGithub] = useState(false)
     const [isHoveringDownload, setHoveringDownload] = useState(false)
 
-    const linkedInIconStyling = { color: isHoveringLinkedIn ? "#0077b5" : "#292524" }
-    const guthubIconStyling = { color: isHoveringGithub ? "#000" : "#292524" }
-    const downloadIconStyling = { color: isHoveringDownload ? "#ff6167" : "#292524" }
+    const linkedInIconStyling = { color: isHoveringLinkedIn ? '#0077b5' : '#292524' }
+    const guthubIconStyling = { color: isHoveringGithub ? '#000' : '#292524' }
+    const downloadIconStyling = { color: isHoveringDownload ? '#ff6167' : '#292524' }
 
     library.add(faSquareLinkedin, faSquareGithub, faFileArrowDown)
 
-    const linkedInIcon = <FontAwesomeIcon 
-        icon={['fab', 'square-linkedin']} 
-        className={iconStyling} 
-        style={linkedInIconStyling} 
-        onMouseEnter={() => {return onIconMouseEnter("linkedin")}} 
-        onMouseLeave={() => {return onIconMouseLeave("linkedin")}} 
-    />
-    const githubIcon = <FontAwesomeIcon 
-        icon={['fab', 'square-github']} 
-        className={iconStyling} 
-        style={guthubIconStyling}
-        onMouseEnter={() => onIconMouseEnter("github")}
-        onMouseLeave={() => onIconMouseLeave("github")}
-    />
-    const downloadIcon = <FontAwesomeIcon 
-        icon={'file-arrow-down'} 
-        className={iconStyling} 
-        style={downloadIconStyling} 
-        onMouseEnter={() => onIconMouseEnter("download")}
-        onMouseLeave={() => onIconMouseLeave("download")}
-    />
+    const linkedInIcon = (
+        <FontAwesomeIcon
+            icon={['fab', 'square-linkedin']}
+            className={iconStyling}
+            style={linkedInIconStyling}
+            onMouseEnter={() => {
+                return onIconMouseEnter('linkedin')
+            }}
+            onMouseLeave={() => {
+                return onIconMouseLeave('linkedin')
+            }}
+        />
+    )
+    const githubIcon = (
+        <FontAwesomeIcon
+            icon={['fab', 'square-github']}
+            className={iconStyling}
+            style={guthubIconStyling}
+            onMouseEnter={() => onIconMouseEnter('github')}
+            onMouseLeave={() => onIconMouseLeave('github')}
+        />
+    )
+    const downloadIcon = (
+        <FontAwesomeIcon
+            icon={'file-arrow-down'}
+            className={iconStyling}
+            style={downloadIconStyling}
+            onMouseEnter={() => onIconMouseEnter('download')}
+            onMouseLeave={() => onIconMouseLeave('download')}
+        />
+    )
 
-    
+    const device = useDeviceDetection()
+
     function onIconMouseEnter(icon) {
-        switch(icon) {
-            case "linkedin":
+        if (device !== 'Desktop') return
+        switch (icon) {
+            case 'linkedin':
                 setHoveringLinkedIn(true)
                 break
-            case "github":
+            case 'github':
                 setHoveringGithub(true)
                 break
-            case "download":
+            case 'download':
                 setHoveringDownload(true)
                 break
         }
     }
 
     function onIconMouseLeave(icon) {
-        switch(icon) {
-            case "linkedin":
+        switch (icon) {
+            case 'linkedin':
                 setHoveringLinkedIn(false)
                 console.log(isHoveringLinkedIn)
                 break
-            case "github":
+            case 'github':
                 setHoveringGithub(false)
                 break
-            case "download":
+            case 'download':
                 setHoveringDownload(false)
                 break
         }
