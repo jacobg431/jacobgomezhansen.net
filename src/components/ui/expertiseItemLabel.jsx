8c669ff97728bab7f1a140fed0a4711838957e56
Jacob Gomez Hansen <jacobg431@gmail.com>
2025-08-26 12:12:12 +0200

    Added more styling to expertise labels

--- src/components/ui/expertiseItemLabel.jsx
+++ src/components/ui/expertiseItemLabel.jsx
@@ -1,7 +1,7 @@
 function ExpertiseItemLabel(props) {
     const title = props.title
-    const expertiseItemStyling = 'flex justify-center items-center px-8 py-6 font-bold bg-gray rounded-xl'
-    const expertiseItemTextStyling = 'text-center text-sm m-0'
+    const expertiseItemStyling = 'flex justify-center items-center boder border-black border-3 px-4 py-3 font-bold rounded-xl duration-400 ease-out hover:border-red hover:text-red hover:shadow-2xl'
+    const expertiseItemTextStyling = 'text-center text-sm m-0 cursor-default'
 
     return (
         <>
