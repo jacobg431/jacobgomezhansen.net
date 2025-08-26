f9ee02f6d42ebc4d5f5acc8f2ffb0883c15f402c
Jacob Gomez Hansen <jacobg431@gmail.com>
2025-08-26 13:21:22 +0200

    Implemented draggable expertise labels

--- src/components/items/expertiseItem.jsx
+++ src/components/items/expertiseItem.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import { DragDropContext, Draggable, Droppable } from '@hello-pangea/dnd'
 import ExpertiseItemLabel from '../ui/expertiseItemLabel'
 
 function ExpertiseItem(props) {
@@ -7,14 +9,39 @@ function ExpertiseItem(props) {
     const expertiseTitleStyling = 'text-md 2xs:text-xl font-bold mb-6'
     const expertiseLabelsContainerStyling = 'flex flex-wrap gap-6'
 
+    const [labels, setLabels] = useState(labelList)
+
+    const handleDragEnd = (result) => {
+        if (!result.destination) return
+
+        const updatedLabels = Array.from(labels)
+        const [moved] = updatedLabels.splice(result.source.index, 1)
+        updatedLabels.splice(result.destination.index, 0, moved)
+
+        setLabels(updatedLabels)
+    }
+
     return (
         <div className={expertiseItemStyling}>
             <h4 className={expertiseTitleStyling}>{title}</h4>
-            <div className={expertiseLabelsContainerStyling}>
-                {labelList.map((label, index) => (
-                    <ExpertiseItemLabel key={index} title={label} />
-                ))}
-            </div>
+            <DragDropContext onDragEnd={handleDragEnd}>
+                <Droppable droppableId='labels' direction='horizontal'>
+                    {(provided) => (
+                        <div className={expertiseLabelsContainerStyling} ref={provided.innerRef} {...provided.droppableProps}>
+                            {labels.map((label, index) => (
+                                <Draggable key={label} draggableId={label} index={index}>
+                                    {(provided) => (
+                                        <div ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
+                                            <ExpertiseItemLabel title={label} />
+                                        </div>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
         </div>
     )
 }
