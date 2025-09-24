8716ceec5f7ada24a3fbb3e5993f703d38d50fd5
Jacob Gomez Hansen <jacobg431@gmail.com>
2025-09-24 18:37:23 +0200

    Library items stored in json file

--- src/components/items/libraryItem.jsx
+++ src/components/items/libraryItem.jsx
@@ -1,6 +1,6 @@
 function LibraryItem(props) {
-    const title = props.title
-    const description = props.description
+    const title = props.item.title
+    const description = props.item.description
     const libraryItemStyling =
         'flex flex-col xs:flex-row p-6 gap-6 text-center xs:text-left items-center bg-gray rounded-xl'
     const libraryItemImageStyling = 'min-w-20 h-24 bg-black'
--- src/components/sections/library.jsx
+++ src/components/sections/library.jsx
@@ -1,4 +1,5 @@
 import LibraryItem from '../items/libraryItem'
+import libraryData from '../../data/libraryData.json'
 
 function Library() {
     const librarySectionStyling = 'flex justify-center px-4 2xs:px-10 sm:px-20 lg:px-40 bg-white'
@@ -6,6 +7,7 @@ function Library() {
     const libraryTitleStyling = 'text-3xl font-bold mb-6'
     const libraryItemsContainerStyling =
         'grid grid-cols-[repeat(auto-fit,_minmax(16rem,_1fr))] xs:grid-cols-[repeat(auto-fit,_minmax(24rem,_1fr))] gap-6'
+    const libraryItemList = libraryData.itemList
 
     return (
         <>
@@ -13,18 +15,9 @@ function Library() {
                 <div className={libraryInnerWrapperStyling}>
                     <h2 className={libraryTitleStyling}>Library</h2>
                     <div className={libraryItemsContainerStyling}>
-                        <LibraryItem
-                            title="C# and .NET"
-                            description="Lorem ipsum dolor sit amet consectetur adipiscing elit."
-                        />
-                        <LibraryItem
-                            title="Full Stack Development with Spring Boot 3 and React"
-                            description="Lorem ipsum dolor sit amet consectetur adipiscing elit."
-                        />
-                        <LibraryItem
-                            title="The Data Warehouse Toolkit"
-                            description="Lorem ipsum dolor sit amet consectetur adipiscing elit."
-                        />
+                        {libraryItemList.map((item) => (
+                            <LibraryItem key={item.key} item={item} />
+                        ))}
                     </div>
                 </div>
             </section>
