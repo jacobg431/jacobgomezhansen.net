786ef90308f132d91058cf2f465f350016ca1517
Jacob Gomez Hansen <jacobg431@gmail.com>
2025-10-31 12:38:04 +0100

    Added helper function in vite config file to ensure certificate files only read in real prod env

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,24 @@ import process from 'process'
 
 const isProd = process.env.NODE_ENV === 'production'
 
+function getHttpsConfig() {
+    const keyPath = '/certs/privkey.pem'
+    const certPath = '/certs/fullchain.pem'
+
+    if (isProd && fs.existsSync(keyPath) && fs.existsSync(certPath)) {
+        return {
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certPath),
+        }
+    }
+
+    return false
+}
+
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     preview: {
         allowedHosts: ['jacobgomezhansen.net'],
-        https: isProd
-            ? {
-                  key: fs.readFileSync('/certs/privkey.pem'),
-                  cert: fs.readFileSync('/certs/fullchain.pem'),
-              }
-            : false,
+        https: getHttpsConfig(),
     },
 })
