4ad1015dd4fdc71771ec8df4baa82bde3563b44e
Jacob Gomez Hansen <jacobg431@gmail.com>
2025-10-31 12:08:51 +0100

    Updated docker-deploy and vite config files

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import fs from 'fs'
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     preview: {
-        allowedHosts: ['jacobgomezhansen.net', 'jacob.begby.net'],
+        allowedHosts: ['jacobgomezhansen.net'],
+        https: {
+            key: fs.readFileSync('/certs/privkey.pem'),
+            cert: fs.readFileSync('/certs/fullchain.pem'),
+        },
     },
 })
