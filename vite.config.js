66ffb5286b86fdc9320c7485e7387882dbfff2a5
Jacob Gomez Hansen <jacobg431@gmail.com>
2025-10-31 12:26:35 +0100

    Added production env check in vite config

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import fs from 'fs'
+import process from 'process'
+
+const isProd = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     preview: {
         allowedHosts: ['jacobgomezhansen.net'],
-        https: {
-            key: fs.readFileSync('/certs/privkey.pem'),
-            cert: fs.readFileSync('/certs/fullchain.pem'),
-        },
+        https: isProd
+            ? {
+                  key: fs.readFileSync('/certs/privkey.pem'),
+                  cert: fs.readFileSync('/certs/fullchain.pem'),
+              }
+            : false,
     },
 })
